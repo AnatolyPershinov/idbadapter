fe927c974881cd22fc5612324bbe97811c92b3c4
Anatoly <anatolypershinov@gmail.com>
2023-05-29 11:37:20 +0300

    add function for getting names

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ version = '1.3'
 long_description = "Сache module for batch loading of data"
 
 setup(name='idbadapter',
-      version='1.3',
+      version='1.4',
       description='Сache module for batch loading of data',
       long_description=long_description,
       url="https://github.com/AnatolyPershinov/gpn_cache_module",
